5c4f4a178073aeff2c124a2c975237e0286f3162
Boelensman1 <me@wiggerboelens.com>
2017-04-10 13:27:55 +0200

    cleanup initModels
    Signed-off-by: Boelensman1 <me@wiggerboelens.com>

--- test/models/initModels.js
+++ test/models/initModels.js
@@ -1,11 +1,7 @@
 const config = require('config')
+const Knex = require('knex')
 
 module.exports = () => {
-  const Knex = require('knex')
-
   const knex = new Knex(config.knex)
-  // Bind all Models to a knex instance. If you only have one database in
-  // your server this is all you have to do. For multi database systems, see
-  // the Model.bindKnex method.
   return knex.migrate.latest().then(() => (knex))
 }
