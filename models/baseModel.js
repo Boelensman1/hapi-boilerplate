f853fc008203b7137d66d0581da0b2f0667f83b5
Boelensman1 <me@wiggerboelens.com>
2017-04-10 22:57:55 +0200

    Add a validator that runs when inserting into db
    Signed-off-by: Boelensman1 <me@wiggerboelens.com>

--- models/baseModel.js
+++ models/baseModel.js
@@ -1,6 +1,38 @@
 const Model = require('objection').Model
+const Joi = require('joi')
+const Validator = require('objection').Validator
 
+
+class MyCustomValidator extends Validator {
+  validate(args) { // eslint-disable-line class-methods-use-this
+    // The model instance. May be empty at this point.
+    const model = args.model
+
+    // The properties to validate. After validation these values will
+    // be merged into `model` by objection.
+    const json = args.json
+
+    // Do your validation here and throw any exception if the
+    // validation fails.
+    const joiSchema = model.constructor.schema
+    // if no schema has been defined, just return immediatly
+    if (!joiSchema) { return json }
+    const result = Joi.validate(json, joiSchema)
+    if (result.error) {
+      throw result.error
+    }
+
+    return result.value
+  }
+}
+
+// Override the `createValidator` method of a `Model` to use the
+// custom validator.
 class BaseModel extends Model {
+  static createValidator() {
+    return new MyCustomValidator()
+  }
+
   $beforeInsert() {
     this.createdAt = new Date().toISOString()
   }
