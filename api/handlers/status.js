5d2e7f09074007f742d6d5501b5e2d12f010daca
Boelensman1 <me@wiggerboelens.com>
2017-04-10 09:57:41 +0200

    Use array of strings instead of multiline string
    Signed-off-by: Boelensman1 <me@wiggerboelens.com>

--- api/handlers/status.js
+++ api/handlers/status.js
@@ -2,8 +2,8 @@ module.exports = {
   description: 'Simple endpoint you can use to see if the server is up',
   tags: ['status'],
   notes: [
-    `Does not actually check the status,
-    will just always return OK (if the server is running of course)`,
+    'Does not actually check the status,',
+    'will just always return OK (if the server is running of course)',
   ],
   handler(request, reply) {
     return reply({ result: 'OK' })
