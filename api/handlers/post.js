1a8c783a9b587ce789203e67dbd8336faf5a120c
Boelensman1 <me@wiggerboelens.com>
2017-04-10 07:52:11 +0200

    Some basic validation on the post
    Signed-off-by: Boelensman1 <me@wiggerboelens.com>

--- api/handlers/post.js
+++ api/handlers/post.js
@@ -1,9 +1,18 @@
+const Joi = require('joi')
 const Post = require('../../models/post')
+
+const queryValidation = {
+  limit: Joi.number().max(50).description('The maximum amount of posts to get'),
+  page: Joi.number().description('Which page of posts to get'),
+}
 
 module.exports = {
   get: {
     description: 'Simple endpoint you can use to see if the server is up',
     tags: ['status'],
+    validate: {
+      query: queryValidation,
+    },
     notes: [
       `Does not actually check the status,
     will just always return OK (if the server is running of course)`,
