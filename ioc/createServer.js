a584cb41466e4a5b8c97066d0b155485bd1d71fb
Boelensman1 <me@wiggerboelens.com>
2019-12-31 18:09:54 +0100

    Move where server config is done
    Signed-off-by: Boelensman1 <me@wiggerboelens.com>

--- ioc/createServer.js
+++ ioc/createServer.js
@@ -8,11 +8,32 @@ const path = require('path')
  * @param {boolean} skipModelsInit Don't initialize the models
  * @returns {Promise} Promise resolving into the hapi server object
  */
-async function createServer(manifest, skipModelsInit) {
+async function createServer(manifest, skipModelsInit, enableAuth) {
   let manifestObject = manifest
   if (manifest.util) {
     // needed as otherwise hapi errors about unknown properties like util / get
     manifestObject = manifest.util.toObject(manifest)
+  }
+
+  // add cors
+  if (!manifestObject.server) {
+    manifestObject.server = {}
+  }
+  if (!manifestObject.server.routes) {
+    manifestObject.server.routes = {}
+  }
+  if (!manifestObject.server.routes.cors) {
+    manifestObject.server.routes.cors = {
+      origin: ['*'],
+      exposedHeaders: ['Content-Range'],
+    }
+  }
+
+  // skip trailing slashes
+  if (!manifestObject.server.router) {
+    manifestObject.server.router = {
+      stripTrailingSlash: true,
+    }
   }
 
   // add error handling
@@ -24,13 +45,26 @@ async function createServer(manifest, skipModelsInit) {
     throw err
   }
 
+  manifestObject.register.plugins.unshift({ plugin: './api' })
+  manifestObject.register.plugins.unshift({ plugin: '@hapi/inert' })
+  manifestObject.register.plugins.unshift({ plugin: '@hapi/vision' })
+  if (enableAuth) {
+    manifestObject.register.plugins.unshift({
+      plugin: 'plugins/initPermissions',
+    })
+    manifestObject.register.plugins.unshift({ plugin: 'plugins/initAuth' })
+    manifestObject.register.plugins.unshift({ plugin: 'hapi-auth-jwt2' })
+  }
+
   if (!skipModelsInit) {
     await this.resolve('knex')
   }
-  return glue.compose(
-    manifestObject,
-    { relativeTo: path.resolve(__dirname, '..') },
-  )
+  return glue.compose(manifestObject, {
+    relativeTo: path.resolve(__dirname, '..'),
+    preRegister: (server) => {
+      server.app.ioc = this
+    },
+  })
 }
 
 module.exports = createServer
